fede6ce9cb15de6a2d6bda22b2b3ebac1353f27a
abouzeyd <mohoumouhamed01@gmail.com>
2025-07-24 15:00:54 +0000

    creation suppression modification utilisateur terminé

--- src/pages/parametrage/liste-utilisateurs/components/FormUtilisateur.jsx
+++ src/pages/parametrage/liste-utilisateurs/components/FormUtilisateur.jsx
@@ -1,80 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { createUtilisateur, getUtilisateur, updateUtilisateur, getUtilisateurs } from '../../../../service/parametrage/utilisateurs';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { setEdition } from '../../../../store/parametrage/utilisateur';
 import Select from 'react-select';
-import { getEcoles } from '../../../../service/parametrage/ecole';
+import useFormUser from './useFormUser';
 
 export default function FormUtilisateur({ handleClose }) {
-  const dispatch = useDispatch();
-  const { createLoading, createError, receiveEditId, valueEdition, utilisateur, receiveId, listeEcoles } = useSelector(
-    (state) => state.utilisateur
-  );
-
-  const [nameUser, setNameUser] = useState('');
-  const [loginUser, setLoginUser] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [selectId, setSelectId] = useState('');
-
-  const handleSubmit = async () => {
-    if (!nameUser.trim() || !loginUser.trim() || !password.trim()) {
-      alert('Veuillez remplir tous les champs');
-      return;
-    }
-    if (valueEdition === '') {
-      const result = await dispatch(createUtilisateur({ nameUser, loginUser, password, email, phone, selectId }));
-
-      if (createUtilisateur.fulfilled.match(result)) {
-        await dispatch(getUtilisateurs());
-        handleClose();
-        setNameUser('');
-        setLoginUser('');
-        setPassword('');
-        setEmail('');
-        setPhone('');
-      }
-    } else if (valueEdition === 'editer') {
-      const result = await dispatch(updateUtilisateur({ nameUser, loginUser, password, email, phone, receiveId, selectId }));
-
-      if (updateUtilisateur.fulfilled.match(result)) {
-        await dispatch(getUtilisateurs());
-        handleClose();
-        setNameUser('');
-        setLoginUser('');
-        setPassword('');
-        setEmail('');
-        setPhone('');
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (valueEdition === 'editer' && receiveEditId) {
-      dispatch(getUtilisateur(receiveEditId));
-    } else {
-      setNameUser('');
-      setLoginUser('');
-      setPassword('');
-      setEmail('');
-      setPhone('');
-    }
-  }, [valueEdition, receiveEditId, dispatch]);
-
-  useEffect(() => {
-    if (valueEdition === 'editer' && utilisateur) {
-      setNameUser(utilisateur.strUtiname || '');
-      setLoginUser(utilisateur.strUtilogin || '');
-      setEmail(utilisateur.strUtimail || '');
-      setPhone(utilisateur.strUtiphone || '');
-      setPassword('****');
-    }
-  }, [utilisateur, valueEdition]);
-
-  useEffect(() => {
-    dispatch(getEcoles());
-  }, []);
+  const {
+    handleSubmit,
+    nameUser,
+    setNameUser,
+    loginUser,
+    setLoginUser,
+    password,
+    setPassword,
+    email,
+    setEmail,
+    phone,
+    setPhone,
+    selectId,
+    setSelectId,
+    createLoading,
+    createError,
+    valueEdition,
+    listeEcoles,
+    user
+  } = useFormUser({ handleClose });
 
   return (
     <div>
@@ -163,10 +112,11 @@ export default function FormUtilisateur({ handleClose }) {
             </label>
             <Select
               options={listeEcoles}
-              value={listeEcoles?.label}
+              value={listeEcoles.find((option) => option.value === selectId)}
               onChange={(option) => {
                 setSelectId(option ? option.value : '');
               }}
+              isDisabled={user === '01' ? false : true}
             />
           </div>
         </div>
